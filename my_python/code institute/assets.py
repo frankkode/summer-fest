e4d2bfb4d427d4db11905651a93469d7c46be9f8
frank <masasimpafra@gmail.com>
2019-08-05 22:36:34 +0200

    first challenge passed

---
+++ my_python/code institute/assets.py
@@ -0,0 +1,32 @@
+
+def even_number_of_evens(numbers):
+     return False  
+     
+
+
+
+
+
+assert even_number_of_evens([]) == False, "no numbers"
+ # assert even_number_of_evens([2]) == False, "one even number"
+ # assert even_number_of_evens([2,4]) == True, "two even numbers"
+ # assert even_number_of_evens([2,3]) == False,"two numbers only one even"
+ # assert even_number_of_evens([2,3,9,10,13,7,8]) == False,"multiple numbers, three even"
+ # assert even_number_of_evens([2,3,9,10,13,7,8,5,12]) == True, "multiple numbers,four even"
+ # assert even_number_of_evens([1,3,9]) == False,"no even number"
+
+print("all test passed!!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
