a23e5c648592cf0e990de316a9af044dc027148a
frank <masasimpafra@gmail.com>
2019-08-05 23:03:38 +0200

    all test passed

--- my_python/code institute/assets.py
+++ my_python/code institute/assets.py
@@ -1,19 +1,34 @@
 
 def even_number_of_evens(numbers):
-     return False  
+     
+     if numbers == []:
+          return False
+     else:
+          evens = 0
+
+     for numbers in numbers:
+          if numbers % 2 == 0:
+               evens += 1
+
+     if evens == 0:
+          return False
+     else:
+          return evens % 2 == 0
+
      
 
 
 
 
 
-assert even_number_of_evens([]) == False, "no numbers"
- # assert even_number_of_evens([2]) == False, "one even number"
- # assert even_number_of_evens([2,4]) == True, "two even numbers"
- # assert even_number_of_evens([2,3]) == False,"two numbers only one even"
- # assert even_number_of_evens([2,3,9,10,13,7,8]) == False,"multiple numbers, three even"
- # assert even_number_of_evens([2,3,9,10,13,7,8,5,12]) == True, "multiple numbers,four even"
- # assert even_number_of_evens([1,3,9]) == False,"no even number"
+assert even_number_of_evens([]) == False, "no numbers" 
+assert even_number_of_evens([2]) == False, "one even number"
+assert even_number_of_evens([2,4]) == True, "two even numbers"
+assert even_number_of_evens([2,3]) == False,"two numbers only one even"
+assert even_number_of_evens([2,3,9,10,13,7,8])== False,"multiple numbers, three even"
+
+assert even_number_of_evens([2,3,9,10,13,7,8,5,12]) == True, "multiple numbers,four even"
+assert even_number_of_evens([1,3,9]) == False,"no even number"
 
 print("all test passed!!")
 
